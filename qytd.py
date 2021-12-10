834308541985c1dbb2d512e0e461a54303b51de6
Pedro Américo Vinhas <92418196+pedroamericovinhas@users.noreply.github.com>
2021-12-10 16:43:53 -0300

    added extra functions
    Functions added:
        sleep via time;
        querysize as a variable;
        option to quit on search query;

--- qytd.py
+++ qytd.py
@@ -1,22 +1,39 @@
 from yt_dlp import YoutubeDL
 from youtube_search import YoutubeSearch
 from re import search
+from time import sleep
+
+ydl_opts = {'format': 'mp4',
+            'quiet': True,
+            'x': True,
+            'audio-format': 'mp3'}
+querysize = 10
+def changeOpt(opt):
+    pass
 def searchVideo():
-    ydl_opts = {'format': 'mp4'}
-    searchTerms = input("Search Query: ")
-    isUrl = search("http", searchTerms)
-    if isUrl:
-        with YoutubeDL(ydl_opts) as ydl:
-            ydl.download(searchTerms)
-        return 0
-    else:      
-        info = YoutubeSearch(searchTerms, max_results=5).to_dict()
-        print("\nVideo selection. Type the video number to continue.\n")
-        for i, video in enumerate(info, start=1):
-	        video["no"] = i
-	        print(f"{video.get('no')}. {video.get('title')} - {video.get('duration')}")
-        selected = (input("\n"))
-        with YoutubeDL(ydl_opts) as ydl:
-            ydl.download("https://www.youtube.com/watch?v=" + info[int(selected)-1].get('id'))    	
-        return 0
-searchVideo()        
+    while True:
+        query = input("\nSearch Query (type 'quit' to quit): ")
+        if query == 'quit' or query == 'q':
+            break
+        if "https://www.youtube.com/watch?v=" in query:
+            with YoutubeDL(ydl_opts) as ydl:
+                ydl.download(query)
+            pass
+        else:
+            info = YoutubeSearch(query, max_results=querysize).to_dict()
+            while True:
+                print("\nVideo selection. Type the video number to continue.\n")
+                for i, video in enumerate(info, start=1):
+                    video["no"] = i
+                    sleep(0.05)
+                    print(f"{video.get('no')}. {video.get('title')} - {video.get('duration')}")
+                selected = (input("\n"))
+                if selected not in range(1, querysize+1):
+                    print(f"please input a number between 1 and {querysize}.")
+                    sleep(1)
+                    pass
+            with YoutubeDL(ydl_opts) as ydl:
+                ydl.download("https://www.youtube.com/watch?v=" + info[int(selected)-1].get('id'))    	
+            break
+if __name__ == '__main__' :
+        searchVideo()    
